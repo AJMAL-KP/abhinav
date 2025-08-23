5eb6fb1c28b69b2655c8e532648de68ea1e09343
AJMAL-KP <ajukp218@gmail.com>
2025-08-23 12:03:14 +0530

    dlll started

---
+++ dlinkedlist.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node * next;
+};
+struct node *head1=NULL;
+struct node * head2=NULL;
+void insertBeginning(struct node **head_ref,value){
+    struct node * newNode= (struct node *) malloc(sizeof(struct node));
+    if(*head==NULL){
+
+    }
+}
